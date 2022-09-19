dcafdc2db3ac6031447fe59f9dadcedbe25f148d
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-09-19 17:07:18 -0300

    Final Round

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import {
   useFonts,
@@ -6,12 +7,48 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core'
+import * as Notifications from 'expo-notifications'
 
 import { Routes } from './src/routes'
 import { Background } from './src/components/Background'
 import { Loading } from './src/components/Loading'
 
+import './src/services/notificationConfig'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
+import { addNotificationResponseReceivedListener } from 'expo-notifications'
+
 export default function App() {
+  const getNotificationsListener = useRef<Subscription>()
+  const responseNotificationsListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  })
+
+  useEffect(() => {
+    getNotificationsListener.current =
+      Notifications.addNotificationReceivedListener(notification => {
+        console.log(notification)
+      })
+    responseNotificationsListener.current =
+      addNotificationResponseReceivedListener(response => console.log(response))
+
+    return () => {
+      if (
+        getNotificationsListener.current &&
+        responseNotificationsListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationsListener.current
+        )
+        Notifications.removeNotificationSubscription(
+          responseNotificationsListener.current
+        )
+      }
+    }
+  }, [])
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,20 +1,22 @@
+import { useEffect, useState } from 'react'
+import { TouchableOpacity, View, Image, FlatList, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRoute, useNavigation } from '@react-navigation/native'
-import { Background } from '../../components/Background'
-import { Entypo } from '@expo/vector-icons'
-
-import logoImg from '../../assets/logo-nlw-esports.png'
-
-import { styles } from './styles'
 import { GameParams } from '../../@types/navigation'
-import { TouchableOpacity, View, Image, FlatList, Text } from 'react-native'
+
+import { Entypo } from '@expo/vector-icons'
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { styles } from './styles'
+
+import { Background } from '../../components/Background'
 import { THEME } from '../../theme'
 import { Heading } from '../../components/Heading'
 import { DuoCard, DuoCardProps } from '../../components/DuoCard'
-import { useEffect, useState } from 'react'
+import { DuoMatch } from '../../components/DuoMatch'
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('')
 
   const navigation = useNavigation()
   const route = useRoute()
@@ -22,6 +24,12 @@ export function Game() {
 
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://10.0.0.192:3333/ads/${adsId}/discord`)
+      .then(response => response.json())
+      .then(data => setDiscordDuoSelected(data.discord))
   }
 
   useEffect(() => {
@@ -57,7 +65,7 @@ export function Game() {
           data={duos}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <DuoCard data={item} onConnect={() => {}} />
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
           )}
           horizontal
           style={styles.containerList}
@@ -70,6 +78,12 @@ export function Game() {
               Não há anúncios publicados ainda.
             </Text>
           )}
+        />
+
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
       </SafeAreaView>
     </Background>
